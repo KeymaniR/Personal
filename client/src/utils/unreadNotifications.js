e4f3586918db65d8304498cc0769619f5963b567
KeymaniR <43597214+KeymaniR@users.noreply.github.com>
2024-05-24 09:28:01 -0500

    Delete client directory

--- client/src/utils/unreadNotifications.js
+++
@@ -1,3 +0,0 @@
-export const unreadNotificationsFunc = (notifications) =>{
-    return notifications.filter((n) => n.isRead === false) //if a notification is not read we will return it to this function
-}
